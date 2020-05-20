bab149019fdc9e05bfc870b764d711d3198a414f
radamou <ramatouadamou@gmail.com>
2020-05-20 13:22:06 +0200

    [ios] remove git dir in ios

---
+++ ios/src/components/Dev/DebugStyle.js
@@ -0,0 +1,22 @@
+import { Platform, StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+    },
+    helpContainer: {
+      marginTop: 15,
+      alignItems: 'center',
+    },
+    helpLink: {
+      paddingVertical: 15,
+    },
+    helpLinkText: {
+      fontSize: 14,
+      color: '#2e78b7',
+    },
+  });  
